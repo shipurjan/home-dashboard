a58be0a1526e56a0c6bad1ff3af3a4d25585d91b
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-12-17 11:16:30 +0100

    feat: 🎸 write a barebone scraper in go (not working yet)

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/chromedp/cdproto v0.0.0-20231205062650-00455a960d61 // indirect
 	github.com/chromedp/chromedp v0.9.3 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -4,26 +4,119 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
-	"strings"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
+type TimetableEntry struct {
+	lineNumber string
+	departsIn  string
+}
 
-	// run task list
-	var res string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(`https://pkg.go.dev/time`),
-		chromedp.Text(`.Documentation-overview`, &res, chromedp.NodeVisible),
-	)
+func connectToMariaDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "username:password@tcp(localhost:3306)/mydb")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	log.Println(strings.TrimSpace(res))
+	// Ping the MariaDB server to ensure connectivity
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MariaDB!")
+	return db, nil
+}
+
+func createEntry(db *sql.DB, line_number int, departs_in string) error {
+	_, err := db.Exec("INSERT INTO nearest_bus_departures (line_number, departs_in) VALUES (?, ?)", line_number, departs_in)
+	return err
+}
+
+func main() {
+
+	headlessShellURL := "wss://localhost:9222"
+	// create remote allocator
+	allocatorCtx, cancel := chromedp.NewRemoteAllocator(context.Background(), headlessShellURL)
+	defer cancel()
+
+	// create context
+	ctx, cancel := chromedp.NewContext(allocatorCtx)
+	defer cancel()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("Gracefully shutting down...")
+		cancel()
+		os.Exit(1)
+	}()
+
+	for {
+		var timeTables []*cdp.Node
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(`https://rozklady.bielsko.pl/#stop/2365/50/NO/`),
+			chromedp.Nodes(`div#stoppoint-realtime-entries-box`, &timeTables, chromedp.ByQuery),
+		)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		for _, node := range timeTables {
+			log.Println(node.NodeValue)
+
+		}
+
+		if len(timeTables) != 1 {
+			log.Fatal("Error:", "Expected 1 timetable entry")
+		}
+
+		var timeTable = timeTables[0]
+
+		var timetableEntries []*cdp.Node
+		// run task list
+		err = chromedp.Run(ctx,
+			chromedp.Nodes(`span.stoppoint-ascending--entry`, &timetableEntries, chromedp.ByQueryAll, chromedp.FromNode(timeTable)),
+		)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		log.Println("Found", len(timetableEntries), "timetable entries")
+
+		var timetable []TimetableEntry
+		for _, node := range timetableEntries {
+			var (
+				departsIn  string
+				lineNumber string
+			)
+			err = chromedp.Run(ctx,
+				chromedp.TextContent("span.stoppoint-ascending--entry_time", &departsIn, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("span.stoppoint-ascending--entry_line", &lineNumber, chromedp.ByQuery, chromedp.FromNode(node)),
+			)
+
+			if err != nil {
+				log.Fatal("Error:", err)
+			}
+
+			timetableEntry := TimetableEntry{}
+			timetableEntry.departsIn = departsIn
+			timetableEntry.lineNumber = lineNumber
+
+			timetable = append(timetable, timetableEntry)
+		}
+
+		log.Println(timetable)
+		time.Sleep(1 * time.Second) // wait 15 sec
+	}
 }
